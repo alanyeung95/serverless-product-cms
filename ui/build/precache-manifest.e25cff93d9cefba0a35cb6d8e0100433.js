5bd872a4c708c6f88b902d79aa7f2141f9ce4a74
Alan Yeung <alanyeung1995@gmail.com>
2020-09-06 23:02:41 +0800

    chore: Add build folder

---
+++ ui/build/precache-manifest.e25cff93d9cefba0a35cb6d8e0100433.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3ea77aad9a14bffebe5",
+    "url": "/static/css/main.c96d4155.chunk.css"
+  },
+  {
+    "revision": "c3ea77aad9a14bffebe5",
+    "url": "/static/js/main.c3ea77aa.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "34372c3f89d9a38751f4",
+    "url": "/static/css/2.f0a40dbb.chunk.css"
+  },
+  {
+    "revision": "34372c3f89d9a38751f4",
+    "url": "/static/js/2.34372c3f.chunk.js"
+  },
+  {
+    "revision": "5827b7f229b792fe7e5a7b18a79e7d61",
+    "url": "/index.html"
+  }
+];
