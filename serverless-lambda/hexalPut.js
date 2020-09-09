a454070129d4a1facf4ae0819612a3668c64a401
Alan Yeung <alanyeung1995@gmail.com>
2020-09-10 00:35:36 +0800

    feat: Send event to SNS when user add new product

--- serverless-lambda/hexalPut.js
+++ serverless-lambda/hexalPut.js
@@ -9,6 +9,12 @@ exports.handler = async (event, context) => {
 
   const { id, productname } = JSON.parse(event.body);
 
+  const messageParams = {
+    Message: productname,
+    TopicArn: "arn:aws:sns:us-east-2:719537100027:demo-product-cms-put-product",
+  };
+  AWS.config.update({ region: "us-east-2" });
+
   const params = {
     TableName: "Products",
     Item: {
@@ -17,10 +23,25 @@ exports.handler = async (event, context) => {
     },
   };
 
+  var publishTextPromise = new AWS.SNS({ apiVersion: "2010-03-31" })
+    .publish(messageParams)
+    .promise();
+
   try {
     const data = await documentClient.put(params).promise();
     responseBody = JSON.stringify(data);
     statusCode = 201;
+
+    publishTextPromise
+      .then(function (data) {
+        console.log(
+          `Message ${messageParams.Message} send sent to the topic ${messageParams.TopicArn}`
+        );
+        console.log("MessageID is " + data.MessageId);
+      })
+      .catch(function (err) {
+        console.error(err, err.stack);
+      });
   } catch (err) {
     responseBody = `Unable to put product: ${err}`;
     statusCode = 403;
